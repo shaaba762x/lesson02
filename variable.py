bbfcf15b0feb14c151afa9ee5f3453e9d88dd94e
shaaba762x <shaaba777@gmail.com>
2024-09-15 15:51:57 +0300

    Задание 2 Переменные

---
+++ variable.py
@@ -0,0 +1,5 @@
+_number_of_completed_DZ = 12
+_number_of_hours_spent = 1.5
+_course_name = 'Python'
+_time_for_one_task = (_number_of_hours_spent / _number_of_completed_DZ)
+print('Курс:', _course_name, 'всего задач:', _number_of_completed_DZ, ',затрачено часов:', _number_of_hours_spent, 'среднее время выполнения', _time_for_one_task, 'часа.')

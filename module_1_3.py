e7a9402f8654e4c3c900c754fac9155cc3284bda
shaaba762x <shaaba777@gmail.com>
2024-09-15 20:41:35 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,7 @@ name = 'Batman'
 print('Name', name)
 age = 52
 print('Age', age)
-new_age = 114
-age = new_age
+age = age + 3
 print('New age', age)
 is_student = True
 print('is_student', is_student)

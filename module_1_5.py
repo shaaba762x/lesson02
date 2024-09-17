8fcc8c29691a4dddfc8585dee04923a0c4845a2e
shaaba762x <shaaba777@gmail.com>
2024-09-17 23:31:00 +0300

    Зад 5 Неизменяемые и изменяемые объекты. Кортежи

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+name_ = 'Lora'
+age_ = [104, 101, 212]
+length_ = 49.5
+is_student_ = True
+immutable_var = name_, age_, length_, is_student_
+print(immutable_var)
+immutable_var [1][1] = (69)
+print(immutable_var)
+mutable_list = name_, is_student_, age_, length_
+print(mutable_list)
+mutable_list [2][0] = (42)
+print(mutable_list)

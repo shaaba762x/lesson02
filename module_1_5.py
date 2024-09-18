00229237e0f7284fde9803e1716b136118ff832a
shaaba762x <shaaba777@gmail.com>
2024-09-18 15:04:16 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -6,7 +6,8 @@ immutable_var = name_, age_, length_, is_student_
 print(immutable_var)
 immutable_var [1][1] = (69)
 print(immutable_var)
-mutable_list = name_, is_student_, age_, length_
+mutable_list = [name_, is_student_, age_, length_]
 print(mutable_list)
+mutable_list [name_] = 'Palmer'
 mutable_list [2][0] = (42)
 print(mutable_list)

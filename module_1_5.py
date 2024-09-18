36817802558e922c9a0622e81ffc7b324ba9a607
shaaba762x <shaaba777@gmail.com>
2024-09-18 15:07:51 +0300

    Update module_1_5.py
    Изменение с кортежа на список

--- module_1_5.py
+++ module_1_5.py
@@ -1,13 +1,14 @@
 name_ = 'Lora'
 age_ = [104, 101, 212]
 length_ = 49.5
-is_student_ = True
+is_student_ = (age_[0] > length_)
 immutable_var = name_, age_, length_, is_student_
 print(immutable_var)
-immutable_var [1][1] = (69)
+immutable_var [1][0] = (12)
+is_student_ = (age_[0] > length_)
 print(immutable_var)
 mutable_list = [name_, is_student_, age_, length_]
 print(mutable_list)
-mutable_list [name_] = 'Palmer'
+mutable_list [0] = 'Palmer'
 mutable_list [2][0] = (42)
 print(mutable_list)
